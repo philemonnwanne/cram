2951275a539c0aea377ab2e5cf60b5cc8c54d9a8
philemonnwanne <philemonnwanne@gmail.com>
2023-09-25 03:09:03 +0100

    update cluster module output

--- terraform/modules/cluster/outputs.tf
+++ terraform/modules/cluster/outputs.tf
@@ -2,3 +2,8 @@ output "cluster_name" {
   description = "name of the cluster"
   value       = aws_ecs_cluster.tripvibe.name
 }
+
+output "cluster_id" {
+  description = "ID of the cluster"
+  value       = aws_ecs_cluster.tripvibe.id
+}
